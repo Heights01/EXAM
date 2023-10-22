6d6b4783f8265482b51a279e4b0f4ec908448491
Emmanuel O. Heights <emmanuelheights6@gmail.com>
2023-10-22 06:50:44 +0100

    2nd_Sem_EXAM

--- bash-script.sh
+++ bash-script.sh
@@ -145,11 +145,11 @@ echo "Password:   $PASS"
 
 #########################################################
 
-sudo sed -i 's/DB_DATABASE=laravel/DB_DATABASE=teddi/' /var/www/html/laravel/.env
+sudo sed -i 's/DB_DATABASE=laravel/DB_DATABASE=heights/' /var/www/html/laravel/.env
 
-sudo sed -i 's/DB_USERNAME=root/DB_USERNAME=teddi/' /var/www/html/laravel/.env
+sudo sed -i 's/DB_USERNAME=root/DB_USERNAME=heights/' /var/www/html/laravel/.env
 
-sudo sed -i 's/DB_PASSWORD=/DB_PASSWORD=teddi90/' /var/www/html/laravel/.env
+sudo sed -i 's/DB_PASSWORD=/DB_PASSWORD=heights90/' /var/www/html/laravel/.env
 
 php artisan config:cache
 
